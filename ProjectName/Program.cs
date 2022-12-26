fa04249bdb517fcd0e1853a9375468225090f93d
Chris Loveless <chriskloveless@gmail.com>
2022-12-26 12:05:08 -0800

    update template

--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+// using Microsoft.EntityFrameworkCore;
+// using ProjectName.Models;
 
 namespace ProjectName
 {
@@ -7,24 +9,24 @@ namespace ProjectName
   {
     static void Main(string[] args)
     {
+
       WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
       builder.Services.AddControllersWithViews();
 
+      // builder.Services.AddDbContext<ProjectNameContext>(dbContextOptions => dbContextOptions.UseMySql(builder.Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])));
+
       WebApplication app = builder.Build();
 
-      app.UseDeveloperExceptionPage();
-
+      // app.UseDeveloperExceptionPage();
       app.UseHttpsRedirection();
-
       app.UseStaticFiles();
 
       app.UseRouting();
 
       app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}"
-      );
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}");
 
       app.Run();
     }
